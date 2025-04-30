a7f83230287639c85ab1f619c05072b4b94bd3ee
billshillbill <billshillbill@gmail.com>
2025-04-29 23:19:19 -0700

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,5 +3,14 @@ import os
 import streamlit.web.cli as stcli
 import sys
 
-sys.argv = ["streamlit", "run", "dashboard.py", "--server.port", os.getenv("PORT", "8501"), "--server.address", "0.0.0.0"]
+# 🔧 Tell Streamlit to run the dashboard file
+sys.argv = [
+    "streamlit",
+    "run",
+    "dashboard.py",
+    "--server.port", os.getenv("PORT", "8501"),
+    "--server.address", "0.0.0.0"
+]
+
+# ✅ Execute the command
 sys.exit(stcli.main())
